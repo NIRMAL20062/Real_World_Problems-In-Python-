d6500049990b5ef7fd879e18a7ba02c7ad06fac8
Nirmal Kumar <kumar.nirmal2608@gmail.com>
2024-11-14 14:59:36 +0530

    Question - 2 Solved

--- Kushal_Real_Life_Problems_.py
+++ Kushal_Real_Life_Problems_.py
@@ -1,6 +1,6 @@
-# A teacher has 30 students in a class. Each student has a list of 5 exam scores. Write a program to calculate and display the average score of each student.
+#Q1. A teacher has 30 students in a class. Each student has a list of 5 exam scores. Write a program to calculate and display the average score of each student.
 
-students_scores = [
+""" students_scores = [
     [85, 90, 78, 92, 88],  # Scores for student 1
     [75, 80, 72, 85, 90],  # Scores for student 2
     [88, 94, 90, 86, 85],  # Scores for student 3
@@ -14,8 +14,22 @@ for i in range(len(students_scores)):
                 avg=total/len(students_scores[i])
             lists.append(avg)
             
-print(lists)
+print(lists) """
 
 
 """ x=[85, 90, 78, 92, 88]
 print(sum(x)) """
+
+
+
+# Q2 . A store has 10 different items. The store manager wants to apply a 10% discount on every item that costs more than INR 50. Write a program to display the updated prices for each item.
+
+dicts={'Notebook':60,"Iphone":100,"Mobile":30}
+dict1={}
+for i in dicts:
+    x=(dicts[i])
+    if x>50: 
+        discounted_price=((dicts[i] )-(((dicts[i])*(10/100))))
+        dict1[i]=discounted_price
+print(dict1)
+
