441b6d72d89834f180a0ab7694579d335cd8151b
Nirmal Kumar <kumar.nirmal2608@gmail.com>
2024-11-14 08:38:43 +0530

    Python file added

---
+++ Kushal_Real_Life_Problems_.py
@@ -0,0 +1,11 @@
+# A teacher has 30 students in a class. Each student has a list of 5 exam scores. Write a program to calculate and display the average score of each student.
+
+students_scores = [
+    [85, 90, 78, 92, 88],  # Scores for student 1
+    [75, 80, 72, 85, 90],  # Scores for student 2
+    [88, 94, 90, 86, 85],  # Scores for student 3
+    # Add more lists for each of the 30 students
+    # ...
+]
+for i in range(len(students_scores)):
+    
